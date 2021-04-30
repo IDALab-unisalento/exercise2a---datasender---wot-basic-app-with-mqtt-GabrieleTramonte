ecd7443012df18f0236bb6fce1cca859fde34030
Gabriele Tramonte <gabry.t92@gmail.com>
2021-04-30 17:51:14 +0200

    using mqtt

--- firstApp.js
+++ firstApp.js
@@ -1,5 +1,6 @@
 //const sensorLib = require('node-dht-sensor'); // include existing module called 'node-dht-sensor'
 const http = require('http')
+const mqtt = require('mqtt')
 
 // Setup sensor, exit if failed
 /*
@@ -13,6 +14,8 @@ if (!sensorLib.initialize(sensorType, sensorPin))
 }
 */
 // initialize the request
+
+const client = mqtt.connect("mqtt://broker.hivemq.com", {clientId:"mqttjs_gabrieletramonte"})
 
 // Automatically update sensor value every 2 seconds
 //we use a nested function (function inside another function)
@@ -30,7 +33,7 @@ setInterval(function() {
 
     const options = {
 
-        hostname: 'localhost',
+        hostname: '192.168.1.232',
         port: 3000,
         path: '/temperature',
         method: 'POST',
@@ -40,7 +43,9 @@ setInterval(function() {
         }
     }
 
-    const req = http.request(options, res => {
+    client.publish("test-topic-handson/gabriele",data);
+
+   /* const req = http.request(options, res => {
         console.log(`statusCode: ${res.statusCode}`);
 
         //define the callback function that will print the result of the request in case of success
@@ -52,8 +57,8 @@ setInterval(function() {
         req.on('error', error => {
             console.error(error);
         })
-    })
+    })*/
     //send the request
-    req.write(data);
-    req.end();
+   // req.write(data);
+    //req.end();
 }, 2000);
